d0feed72e745a1efe475fbace32db919d089308f
Olo <jszczep@gmail.com>
2017-01-18 21:15:26 +0100

    settings changes

--- SPage/settings.py
+++ SPage/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'olooo682@gmail.com'
-EMAIL_HOST_PASSWORD = 'oloo1111'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
