285c4df57d3d4abb7946ccc704ad219227a0a0c6
Olo <jszczep@gmail.com>
2017-01-18 20:47:20 +0100

    changes in staticfiles settings for heroku

--- SPage/settings.py
+++ SPage/settings.py
@@ -129,29 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
-
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'collector', 'static'),
 )
-
-
-
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
-# ]
-
 
 # django registration redux
 ACCOUNT_ACTIVATION_DAYS = 7
