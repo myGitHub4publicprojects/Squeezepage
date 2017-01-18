92bcb99ed29c93fa6ca5cb212d55bd84f5f29af8
Olo <jszczep@gmail.com>
2017-01-18 08:59:04 +0100

    additional files and changes for Heroku

--- SPage/settings.py
+++ SPage/settings.py
@@ -144,3 +144,19 @@ LOGIN_REDIRECT_URL = '/'
 
 # for 'django.contrib.sites'
 SITE_ID = 1
+
+
+# for Heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
